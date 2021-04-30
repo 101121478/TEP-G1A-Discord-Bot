8deaa1a0a62f36b47778b0a44fe15c90dfb15f97
Luke Parker <48575448+101121478@users.noreply.github.com>
2021-04-30 17:37:26 +1000

    Renamed to disbot.py, added code for censoring a message based on given words from badwords.txt

---
+++ disbot.py
@@ -0,0 +1,33 @@
+# bot.py
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+
+# Open badwords.txt file and read each line into an array
+with open("badwords.txt") as file:
+    bad_words = file.read().split()
+
+# When a message is sent then bot will check it against the bad_words array 
+# to see if there are any words in the message that are blacklisted
+@bot.event
+async def on_message(message):
+    for bad_word in bad_words:
+        if bad_word in message.content.lower():
+            await message.channel.send("{}, your message has been censored.".format(message.author.mention))
+            await message.delete()
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+bot.run(TOKEN)
