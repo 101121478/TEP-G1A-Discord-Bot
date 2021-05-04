ea8bc5b3cbe195f609fad4512e9365842afc94eb
Luke Parker <48575448+101121478@users.noreply.github.com>
2021-05-04 18:39:44 +1000

    Added functions for interacting with the an sql database including display, add and delete

--- disbot.py
+++ disbot.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import mysql.connector
 
 import discord
 from discord.ext import commands
@@ -8,9 +9,38 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+dbHost = os.getenv('dbHost')
+dbUser = os.getenv('dbUser')
+dbPassword = os.getenv('dbPassword')
+db = os.getenv('database')
+
 client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
+# Initialise connection to SQL database with details from .env file
+mydb = mysql.connector.connect(
+    host=dbHost,
+    user=dbUser,
+    password=dbPassword,
+    database=db
+)
+mycursor = mydb.cursor()
+
+def create_topics_table():
+    mycursor.execute("CREATE TABLE topics (topic VARCHAR(30), count INT DEFAULT 0)")
+    mydb.commit()
+
+def does_topics_exists():
+    mycursor.execute("SHOW TABLES LIKE 'topics'")
+    result = mycursor.fetchone()
+    if result:
+        return True
+    else:
+        return False
+
+if not does_topics_exists():
+    print("Table does not exist")
+    create_topics_table()
 
 # Open badwords.txt file and read each line into an array
 with open("badwords.txt") as file:
@@ -29,6 +59,62 @@ async def on_message(message):
         await message.channel.send("{}, your message has been censored.".format(message.author.mention))
     
     await bot.process_commands(message)
+
+# Will retrieve the concepts/topics and the number of times they have been mentioned in the server
+@bot.command()
+async def get_common_topics(ctx):
+    topics = []
+
+    mycursor.execute("SELECT * FROM topics")
+    result = mycursor.fetchall()
+
+    if result:
+        for topic in result:
+            print(str(topic))
+            topics.append(str(topic))
+        
+        message = '\n'.join(topics)
+        print(message)
+        await ctx.channel.send(message)
+    else:
+        await ctx.channel.send("No topics in the table to display. Add a topic using the command !add_topic topic")
+
+# Will add the new topic that the user enters when this command is entered e.g. !add_new_topic Python will add Python
+@bot.command()
+async def add_topic(ctx, topic):
+    sql = "SELECT * FROM topics WHERE topic = '{}'".format(topic)
+
+    mycursor.execute(sql)
+    result = mycursor.fetchall()
+
+    if result:
+        await ctx.channel.send("Topic: {} already exists in 'topics' table in database: {}.".format(topic, db))
+    else:
+        sql = "INSERT INTO topics (topic, count) VALUES (%s, %s)"
+        val = (topic, 0)
+        
+        mycursor.execute(sql, val)
+        mydb.commit()
+
+        await ctx.channel.send("Added topic: {} to the 'topics' table in database: {}.".format(topic, db))
+
+# Deletes the topic entered by the user from the database
+@bot.command()
+async def delete_topic(ctx, topic):
+    sql = "SELECT * FROM topics WHERE topic = '{}'".format(topic)
+
+    mycursor.execute(sql)
+    result = mycursor.fetchall()
+
+    if not result:
+        await ctx.channel.send("No topic: {} in 'topics' table in database: {}.".format(topic, db))
+    else:
+        sql = "DELETE FROM topics WHERE topic = '{}'".format(topic)
+
+        mycursor.execute(sql)
+        mydb.commit()
+
+        await ctx.channel.send("Topic: {} has been deleted from 'topics' table in database: {}.".format(topic, db))
 
 @bot.event
 async def on_ready():
