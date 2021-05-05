6941493ec0142d3d4ac5ad3c5efdd5b90ab78625
Luke Parker <luke32246@gmail.com>
2021-05-05 18:25:51 +1000

    Bot will now check messages and count the times that specified topics are mentioned

--- disbot.py
+++ disbot.py
@@ -48,8 +48,7 @@ with open("badwords.txt") as file:
 
 # When a message is sent then bot will check it against the bad_words array 
 # to see if there are any words in the message that are blacklisted
-@bot.event
-async def on_message(message):
+async def filter_messages(message):
     result = False;
     for bad_word in bad_words:
         if bad_word in message.content.lower():
@@ -59,6 +58,29 @@ async def on_message(message):
         await message.channel.send("{}, your message has been censored.".format(message.author.mention))
     
     await bot.process_commands(message)
+
+# Retreives all topics from the topics table and checks if any of them
+# are in the message, if so then update the count for that topic by 1
+def check_for_common_topic(message):
+    mycursor.execute("SELECT * FROM topics")
+    result = mycursor.fetchall()
+    count = 0
+
+    for topic in result:
+        if topic[0] in message.content.lower() and message.author != bot.user and "!add_topic" not in message.content.lower():
+            count = topic[1] + 1
+            sql = "UPDATE topics SET count = {} WHERE topic = '{}'".format(count, topic[0])
+            mycursor.execute(sql)
+    
+    mydb.commit()
+
+
+
+@bot.event
+async def on_message(message):
+    await filter_messages(message)
+    check_for_common_topic(message)
+
 
 # Will retrieve the concepts/topics and the number of times they have been mentioned in the server
 @bot.command()
@@ -70,11 +92,9 @@ async def get_common_topics(ctx):
 
     if result:
         for topic in result:
-            print(str(topic))
             topics.append(str(topic))
         
         message = '\n'.join(topics)
-        print(message)
         await ctx.channel.send(message)
     else:
         await ctx.channel.send("No topics in the table to display. Add a topic using the command !add_topic topic")
@@ -82,6 +102,7 @@ async def get_common_topics(ctx):
 # Will add the new topic that the user enters when this command is entered e.g. !add_new_topic Python will add Python
 @bot.command()
 async def add_topic(ctx, topic):
+    topic = topic.lower()
     sql = "SELECT * FROM topics WHERE topic = '{}'".format(topic)
 
     mycursor.execute(sql)
@@ -101,6 +122,7 @@ async def add_topic(ctx, topic):
 # Deletes the topic entered by the user from the database
 @bot.command()
 async def delete_topic(ctx, topic):
+    topic = topic.lower()
     sql = "SELECT * FROM topics WHERE topic = '{}'".format(topic)
 
     mycursor.execute(sql)
