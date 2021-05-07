eea0d3e62be92f67207cd74c077c0c954e1882cb
renne05 <79634373+renne05@users.noreply.github.com>
2021-05-07 13:17:48 +1000

    added notification for welcoming and reminder
    added notification for welcoming and reminder

--- testDiscordConnection.py
+++ testDiscordConnection.py
@@ -1,16 +1,54 @@
 # bot.py
 import os
-
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+scheduler = AsyncIOScheduler()
+guild = None;
+welcome_channel = None;
+reminder_channel = None;
 
-@client.event
+def in_channel(channel_id):
+    def predicate(ctx):
+        return ctx.message.channel.id == channel_id
+    return commands.check(predicate)
+
+async def func():
+    await bot.wait_until_ready()
+    reminder_channel = bot.get_channel(837522915410968609)
+    await reminder_channel.send("Assignment Reminder")
+
+
+@bot.command()
+async def assignment_reminder(ctx, date, hour, minute):
+    addScheduleByDateHourMinute(date, hour, minute)
+    reminder_channel = bot.get_channel(837522915410968609)
+    await reminder_channel.send("Reminder added")
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
+    print("Ready")
+    # Initializing scheduler
+    scheduler.start()
 
-client.run(TOKEN)
+
+@bot.event
+async def on_member_join(member):
+    guild = bot.get_guild(819751859945996300)
+    welcome_channel = guild.get_channel(819751860398456874)
+    await welcome_channel.send(f'Welcome to the {member.guild}, {member.mention}! :partying_face:')
+
+
+def addScheduleByDateHourMinute(date, hour, minute):
+    scheduler.add_job(func, CronTrigger(hour= hour, day= date, minute= minute))
+
+bot.run(TOKEN)
