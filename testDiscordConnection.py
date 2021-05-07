2d1db0b4636f689ad8540ba0680ddf7ebe606459
Divya19997 <80497624+Divya19997@users.noreply.github.com>
2021-05-07 14:01:30 +1000

    Kicking and Banning is working!

--- testDiscordConnection.py
+++ testDiscordConnection.py
@@ -16,17 +16,18 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
-@bot.command()
-async def ban(ctx,member):
-    await bot.ban(member, reason=None)
-    await ctx.channel.send("{} has been banned from the server".format(member))
-
-@bot.command()
-async def kick(ctx,member):
-    await bot.kick(member, reason=None)
-    await ctx.channel.send("{} has been kicked from the server".format(member))
+@bot.command(pass_context = True)
+async def ban(ctx, user: discord.User):
+   await ctx.guild.ban(user)
+   await ctx.channel.send("{} has been banned from the server".format(user))
     
 
-   
+@bot.command(pass_context = True)
+async def kick(ctx, user: discord.User):
+    await ctx.guild.kick(user)
+    await ctx.channel.send("{} has been kicked from the server".format(user))
+
+
+
 
 bot.run(TOKEN)
