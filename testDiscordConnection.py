5621b88ec20f6abc9aa6a06f6b295a7df0cb5ead
Divya19997 <80497624+Divya19997@users.noreply.github.com>
2021-05-07 13:23:38 +1000

    Added kicking and banning

--- testDiscordConnection.py
+++ testDiscordConnection.py
@@ -1,16 +1,32 @@
+
 # bot.py
 import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-client.run(TOKEN)
+@bot.command()
+async def ban(ctx,member):
+    await bot.ban(member, reason=None)
+    await ctx.channel.send("{} has been banned from the server".format(member))
+
+@bot.command()
+async def kick(ctx,member):
+    await bot.kick(member, reason=None)
+    await ctx.channel.send("{} has been kicked from the server".format(member))
+    
+
+   
+
+bot.run(TOKEN)
