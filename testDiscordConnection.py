2197b2b972bea7ae0afe229f4f647917d67748fd
Luke Parker <luke32246@gmail.com>
2021-05-11 17:41:25 +1000

    Added better profanity library for censoring

--- testDiscordConnection.py
+++ testDiscordConnection.py
@@ -2,6 +2,7 @@
 import os
 
 import discord
+from better_profanity import profanity
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -12,21 +13,23 @@ client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
 
+
 # Open badwords.txt file and read each line into an array
-with open("badwords.txt") as file:
-    bad_words = file.read().split()
+with open("censoredWords.txt") as file:
+    censored_words = file.read().split()
 
 # When a message is sent then bot will check it against the bad_words array 
 # to see if there are any words in the message that are blacklisted
 @bot.event
 async def on_message(message):
+    profanity.add_censor_words(censored_words)
+
     result = False;
-    for bad_word in bad_words:
-        if bad_word in message.content.lower():
-            result = True
+    if profanity.contains_profanity(message.content.lower()):
+        result = True
     if result:
         await message.delete()
-        await message.channel.send("{}, your message has been censored.".format(message.author.mention))
+        await message.channel.send("{}, your message has been deleted as it contains inappropriate text.".format(message.author.mention))
     
     await bot.process_commands(message)
 
